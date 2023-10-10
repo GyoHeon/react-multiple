7373cff651a5bf2d5542fd786e521139f6571487
Gyoheon <ghl12078@gmail.com>
2023-10-10 15:46:30 +0900

    Feat: Edit docs

--- apps/docs/src/observer.tsx
+++ apps/docs/src/observer.tsx
@@ -2,45 +2,40 @@ import makeRoot, { observeData } from "react-multiple";
 import ComponentObj from "./components/ComponentObj";
 import "./index.css";
 
-const observedNumber = observeData<CounterState>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-}));
-
-const allReactRoot = document.querySelectorAll("*[data-react]");
-
-const componentsRenderers = [...allReactRoot].map((root) => {
-  const componentName = root.getAttribute(
-    "data-react"
-  ) as keyof typeof ComponentObj;
-
-  const Component = ComponentObj[componentName] || <div>fail!</div>;
-  const props = JSON.parse(root.getAttribute("data-react-props") || "{}") || {};
-
-  const children = (() => {
-    if (root.innerHTML) {
-      if (root.childElementCount === 0) {
-        return root.innerHTML;
-      }
-      props["html"] = root.innerHTML;
-      return;
-    }
-    return;
-  })();
-
-  return makeRoot({
-    root,
-    Component,
-    props: { children, ...props },
-  });
-});
-
-const [buttonRender, displayRender] = componentsRenderers;
-
 type CounterState = {
   count: number;
   increment: () => void;
 };
 
-displayRender({ count: 0 });
-buttonRender({ onClick: () => console.log("sibal") });
+const observedNumber = observeData<CounterState>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+}));
+
+const reactButtonRoot = document.querySelector(
+  "div[data-react=CountButton]"
+) as HTMLDivElement;
+const reactDisplayRoot = document.querySelector(
+  "*[data-react=CountDisplay]"
+) as HTMLDivElement;
+
+// Render button only once
+makeRoot({
+  root: reactButtonRoot,
+  Component: ComponentObj.CountButton,
+  props: {
+    onClick: () => {
+      observedNumber.getState().increment();
+    },
+  },
+});
+
+const displayRender = makeRoot({
+  root: reactDisplayRoot,
+  Component: ComponentObj.CountDisplay,
+  props: { count: observedNumber.getState().count },
+});
+
+observedNumber.setObserver(() => {
+  displayRender({ count: observedNumber.getState().count });
+});
